cfece5d3abe11dd014d3835ffeadd09fcda69ab3
Daniil <krist311@icloud.com>
2019-02-10 18:14:29 +0300

    update MLP and ResNet models, add validation set and train accuracy counting

--- models.py
+++ models.py
@@ -5,36 +5,51 @@ import torch.nn.functional as F
 class MLP(nn.Module):
     def __init__(self):
         super(MLP, self).__init__()
-        self.fc1 = nn.Linear(32 * 32 * 3, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc1 = nn.Linear(32 * 32 * 3, 1000)
+        self.bn1 = nn.BatchNorm1d(1000)
+        self.fc2 = nn.Linear(1000, 500)
+        self.bn2 = nn.BatchNorm1d(500)
+        self.fc3 = nn.Linear(500, 120)
+        self.bn3 = nn.BatchNorm1d(120)
+        self.fc4 = nn.Linear(120, 84)
+        self.bn4 = nn.BatchNorm1d(84)
+        self.fc5 = nn.Linear(84, 10)
 
     def forward(self, x):
         x = x.view(-1, 32 * 32 * 3)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = F.relu(self.bn1(self.fc1(x)))
+        x = F.relu(self.bn2(self.fc2(x)))
+        x = F.relu(self.bn3(self.fc3(x)))
+        x = F.relu(self.bn4(self.fc4(x)))
+        x = self.fc5(x)
         return x
 
 
 class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1):
+    def __init__(self, in_channels, out_channels, padding=1):
         super(ResidualBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=3,
-                               stride=1, padding=1, bias=True)
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3,
+                               stride=1, padding=padding, bias=True)
         self.bn1 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(in_channels, out_channels, kernel_size=3,
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3,
                                stride=1, padding=1, bias=False)
         self.bn2 = nn.BatchNorm2d(out_channels)
+        self.downsample = None
+        if (padding != 1) or (in_channels != out_channels):
+            self.downsample = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, padding=padding, kernel_size=3),
+                nn.BatchNorm2d(out_channels))
 
     def forward(self, x):
         residual = x
         out = self.conv1(x)
-        out = self.bn1(out)
         out = self.relu(out)
+        out = self.bn1(out)
         out = self.conv2(out)
         out = self.bn2(out)
+        if self.downsample:
+            residual = self.downsample(x)
         out += residual
         out = self.relu(out)
         return out
@@ -45,21 +60,29 @@ class ResNet(nn.Module):
         super(ResNet, self).__init__()
         self.conv = nn.Conv2d(3, 16, kernel_size=3,
                               stride=1, padding=1, bias=True)
-        self.bn = nn.BatchNorm2d(16)
         self.relu = nn.ReLU(inplace=True)
-        self.res_block = ResidualBlock(16, 16)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.res_block1 = ResidualBlock(16, 16)
+        self.res_block2 = ResidualBlock(16, 32, padding=0)
+        self.res_block3 = ResidualBlock(32, 64, padding=0)
         self.avg_pool = nn.MaxPool2d(2)
-        self.fc = nn.Linear(16 * 16 * 16, num_classes)
-
-    def make_layer(self, block, out_channels):
-        return block(self.in_channels, out_channels)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.fc1 = nn.Linear(14 * 14 * 64, 500)
+        self.bn3 = nn.BatchNorm1d(500)
+        self.fc2 = nn.Linear(500, num_classes)
 
     def forward(self, x):
         out = self.conv(x)
-        out = self.bn(out)
-        out = self.res_block(out)
-        out = self.avg_pool(out)
-        out = out.view(out.size(0), -1)
-        out = self.fc(out)
-        return out
+        out = self.relu(out)
+        out = self.bn1(out)
+        out = self.res_block1(out)
+        out = self.res_block2(out)
+        out = self.res_block3(out)
 
+        out = self.avg_pool(out)
+        out = self.bn2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.bn3(out)
+        out = self.fc2(out)
+        return out
--- train.py
+++ train.py
@@ -6,7 +6,9 @@ import torchvision
 import torchvision.transforms as transforms
 import torch.optim as optim
 import torch.nn as nn
+import numpy as np
 from tensorboardX import SummaryWriter
+from torch.utils.data import SubsetRandomSampler
 
 from models import ResNet, MLP
 
@@ -16,26 +18,37 @@ def load_data():
         [transforms.ToTensor(),
          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+    train_set = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                             download=True, transform=transform)
+    valid_size = 0.1
+    num_train = len(train_set)
+    indices = list(range(num_train))
+    np.random.seed(3)
+    np.random.shuffle(indices)
+    split = int(np.floor(valid_size * num_train))
+    train_idx, valid_idx = indices[split:], indices[:split]
+    train_sampler = SubsetRandomSampler(train_idx)
+    valid_sampler = SubsetRandomSampler(valid_idx)
+
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=128,
+                                               sampler=train_sampler, num_workers=2)
+    val_loader = torch.utils.data.DataLoader(train_set, batch_size=128,
+                                             sampler=valid_sampler, num_workers=2)
+    test_set = torchvision.datasets.CIFAR10(root='./data', train=False,
                                             download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                              shuffle=True, num_workers=2)
-
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                           download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                             shuffle=False, num_workers=2)
-    return trainloader, testloader
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=128,
+                                              shuffle=False, num_workers=2)
+    return train_loader, val_loader, test_loader
 
 
-def train_model(model, loss_criterion, train_loader, optimizer, num_epochs, tensorboard_writer=None,
-                silent=False, test_loader=None):
+def train_model(model, loss_criterion, train_loader, optimizer, scheduler, num_epochs, tensorboard_writer=None,
+                silent=False, val_loader=None):
     for epoch in range(num_epochs):
         running_loss = 0.0
-        iter = 0
-        for iter_in_batch, data in enumerate(train_loader, 0):
+        iteration = 0
+        scheduler.step()
+        for iter_in_epoch, data in enumerate(train_loader, 0):
             inputs, labels = data
-
             optimizer.zero_grad()
 
             outputs = model(inputs)
@@ -44,26 +57,35 @@ def train_model(model, loss_criterion, train_loader, optimizer, num_epochs, tens
             optimizer.step()
 
             # print statistics
-            current_loss = loss.item()
-            if iter % 2000 == 1999:
-                test_model(model, test_loader, iter=iter,
-                           tensorboard_writer=tensorboard_writer)
-            if tensorboard_writer and iter % 10 == 0:
-                tensorboard_writer.add_scalar('data/loss ', loss.item(), iter)
-                for name, param in model.named_parameters():
-                    tensorboard_writer.add_histogram(name, param.clone().cpu().data.numpy(),
-                                                     iter)
-            if not silent:
-                running_loss += current_loss
-                if iter_in_batch % 2000 == 1999:  # print every 2000 mini-batches
-                    print('[%d, %5d] loss: %.3f' %
-                          (epoch + 1, iter_in_batch + 1, running_loss / 2000))
-                    running_loss = 0.0
-            iter += 1
+            running_loss += loss.item()
+            if iteration % 100 == 99:
+                train_acc = test_model(model, train_loader)
+                val_acc = test_model(model, val_loader)
+                av_loss = running_loss / 100
+                if tensorboard_writer:
+                    write_results(tensorboard_writer, av_loss, iteration, model, train_acc, val_acc)
+                if not silent:
+                    print_results(iter_in_epoch, epoch, av_loss, train_acc, val_acc)
+                running_loss = 0
+            iteration += 1
     print('Finished Training')
 
 
-def test_model(model, test_loader, tensorboard_writer=None, iter=None):
+def print_results(iter, epoch, loss, train_acc, val_acc):
+    print('[%d, %5d] loss: %.3f train_acc: %.3f, val_acc: %.3f' %
+          (epoch + 1, iter + 1, loss, train_acc, val_acc))
+
+
+def write_results(tensorboard_writer, loss, iter, model, train_acc, test_acc):
+    tensorboard_writer.add_scalar('data/loss ', loss, iter)
+    tensorboard_writer.add_scalar('data/train_acc ', test_acc, iter)
+    tensorboard_writer.add_scalar('data/test_acc ', test_acc, iter)
+    for name, param in model.named_parameters():
+        tensorboard_writer.add_histogram(name, param.clone().cpu().data.numpy(),
+                                         iter)
+
+
+def test_model(model, test_loader, print_results=False):
     correct = 0
     total = 0
     with torch.no_grad():
@@ -74,23 +96,23 @@ def test_model(model, test_loader, tensorboard_writer=None, iter=None):
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
     acc = 100 * correct / total
-    if tensorboard_writer:
-        tensorboard_writer.add_scalar('data/test_acc ', acc, iter)
-    print('Accuracy of the network on the 10000 test images: %d %%' % (
-        100 * correct / total))
+    if print_results:
+        print("Test acc: ", acc)
+    return acc
 
 
-def train(model_name, lr, num_epochs):
-    train_loader, test_loader = load_data()
+def train(model_name, lr, num_epochs, weight_decay):
+    train_loader, val_loader, test_loader = load_data()
     model = get_model_by_name(model_name)
     if model:
         writer = SummaryWriter('logs/' + model_name)
         criterion = nn.CrossEntropyLoss()
-        optimizer = optim.Adam(model.parameters(), lr=lr)
-        train_model(model, criterion, train_loader, optimizer, num_epochs=num_epochs,
+        optimizer = optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
+        scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+        train_model(model, criterion, train_loader, optimizer, scheduler, num_epochs=num_epochs,
                     tensorboard_writer=writer,
-                    test_loader=test_loader)
-        test_model(model, test_loader)
+                    val_loader=val_loader)
+        test_model(model, test_loader, print_results=True)
 
 
 def createParser():
@@ -98,6 +120,7 @@ def createParser():
     parser.add_argument('-m', '--model', )
     parser.add_argument('--num_epochs', default=2, type=int)
     parser.add_argument('--learning_rate', default=0.001, type=float)
+    parser.add_argument('--weight_decay', default=1e-5, type=float)
     return parser
 
 
@@ -111,4 +134,4 @@ def get_model_by_name(model_name):
 if __name__ == '__main__':
     parser = createParser()
     args = parser.parse_args(sys.argv[1:])
-    train(args.model, float(args.learning_rate), int(args.num_epochs))
+    train(args.model, args.learning_rate, args.num_epochs, args.weight_decay)
