cfb0193e97daf40fc0bc7b4e0c80c0344e917b92
Daniil <krist311@icloud.com>
2019-02-11 09:57:55 +0300

    increase ResNet acc up to 79%

--- models.py
+++ models.py
@@ -25,27 +25,27 @@ class MLP(nn.Module):
         return x
 
 
+def conv3x3(in_channels, out_channels, stride=1):
+    return nn.Conv2d(in_channels, out_channels, kernel_size=3,
+                     stride=stride, padding=1, bias=False)
+
+
+# Residual block
 class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, padding=1):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
         super(ResidualBlock, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3,
-                               stride=1, padding=padding, bias=True)
+        self.conv1 = conv3x3(in_channels, out_channels, stride)
         self.bn1 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3,
-                               stride=1, padding=1, bias=False)
+        self.conv2 = conv3x3(out_channels, out_channels)
         self.bn2 = nn.BatchNorm2d(out_channels)
-        self.downsample = None
-        if (padding != 1) or (in_channels != out_channels):
-            self.downsample = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, padding=padding, kernel_size=3),
-                nn.BatchNorm2d(out_channels))
+        self.downsample = downsample
 
     def forward(self, x):
         residual = x
         out = self.conv1(x)
-        out = self.relu(out)
         out = self.bn1(out)
+        out = self.relu(out)
         out = self.conv2(out)
         out = self.bn2(out)
         if self.downsample:
@@ -58,31 +58,36 @@ class ResidualBlock(nn.Module):
 class ResNet(nn.Module):
     def __init__(self, num_classes=10):
         super(ResNet, self).__init__()
-        self.conv = nn.Conv2d(3, 16, kernel_size=3,
-                              stride=1, padding=1, bias=True)
+        self.in_channels = 16
+        self.conv = conv3x3(3, 16)
+        self.bn = nn.BatchNorm2d(16)
         self.relu = nn.ReLU(inplace=True)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.res_block1 = ResidualBlock(16, 16)
-        self.res_block2 = ResidualBlock(16, 32, padding=0)
-        self.res_block3 = ResidualBlock(32, 64, padding=0)
-        self.avg_pool = nn.MaxPool2d(2)
-        self.bn2 = nn.BatchNorm2d(64)
-        self.fc1 = nn.Linear(14 * 14 * 64, 500)
-        self.bn3 = nn.BatchNorm1d(500)
-        self.fc2 = nn.Linear(500, num_classes)
+        self.layer1 = self.make_layer(ResidualBlock, 16, blocks=2)
+        self.layer2 = self.make_layer(ResidualBlock, 32, blocks=2, stride=2)
+        self.layer3 = self.make_layer(ResidualBlock, 64, blocks=2, stride=2)
+        self.avg_pool = nn.AvgPool2d(8)
+        self.fc = nn.Linear(64, num_classes)
+
+    def make_layer(self, block, out_channels, blocks, stride=1):
+        downsample = None
+        if (stride != 1) or (self.in_channels != out_channels):
+            downsample = nn.Sequential(
+                conv3x3(self.in_channels, out_channels, stride=stride),
+                nn.BatchNorm2d(out_channels))
+        layers = []
+        layers.append(block(self.in_channels, out_channels, stride, downsample))
+        self.in_channels = out_channels
+        for i in range(1, blocks):
+            layers.append(block(out_channels, out_channels))
+        return nn.Sequential(*layers)
 
     def forward(self, x):
         out = self.conv(x)
-        out = self.relu(out)
-        out = self.bn1(out)
-        out = self.res_block1(out)
-        out = self.res_block2(out)
-        out = self.res_block3(out)
-
+        out = self.bn(out)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
         out = self.avg_pool(out)
-        out = self.bn2(out)
         out = out.view(out.size(0), -1)
-        out = self.fc1(out)
-        out = self.bn3(out)
-        out = self.fc2(out)
+        out = self.fc(out)
         return out
