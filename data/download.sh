789aee1888450b15838851653640f8bfd404a06f
Daniil <krist311@icloud.com>
2019-05-30 12:45:05 +0300

    add datasets with modulation to download.sh

--- data/download.sh
+++ data/download.sh
@@ -17,14 +17,14 @@ gdrive_download 1s55LgFKyybeSueruV8Xvtwh6TE4yJnAb ./audio/Jay_Chou.zip
 gdrive_download 19aXFrhoZdg3Jvf6tDpkPwPFMznuLUW1U ./converted/librosa/TheBeatles.zip
 gdrive_download 1w8Mo2r6ml1v76SiU3MTjgoWiNjFLOH9a ./converted/librosa/USPop179.zip
 gdrive_download 1WbyWA4UcYuMHw7QvXMpb-i1PJrpKDKiZ ./converted/librosa/CaroleKing_Queen.zip
-#download converted by Mauch's algorithm
-gdrive_download 1WzdcHeLeFHrYu_2_NDTEEglfrKLLKc1c ./converted/mauch/TheBeatles.zip
+gdrive_download 19kqa5sZ7YwWd4KHZ8DdyUSP1eDkKnhiL ./converted/librosa/fullmod-1-3.zip
+gdrive_download 1tcB1rcxebdrv88Sv2oUDI3YYL_zlDqoO ./converted/librosa/fullmod24.zip
 #download converted list
 gdrive_download 1wagyEwmqS6rX1CqIptFtzHVNkVHQOoJD ../TheBeatles180List_converted_librosa.txt
-gdrive_download 1ny7Vyir9sEVIMqnFa-3gkPVd7f46wUY1 ../TheBeatles180List_converted_mauch.txt
 gdrive_download 1V5zdvcB50YLfnTlsKyZspvTuVbWghrLE ../USPop179List_converted_librosa.txt
 gdrive_download 1vMFUvgJrAzCsO4PjZ98pYkirGRGNt3Sk ../CaroleKingQueen26List_converted_librosa.txt
 gdrive_download 1m8wC0vAc4p-HbNx68PH1gOfSKv2FE_EU ../FullList386_converted_librosa.txt
+gdrive_download 1HSWo6Wv1fWmWjViN13TERxIgUpCMxoHe ../FullList386_converted_mod_librosa.txt
 #unzip
 unzip -q audio/TheBeatles.zip -d audio
 unzip -q audio/billboard.zip -d audio
@@ -35,7 +35,6 @@ unzip -q audio/Jay_Chou.zip -d audio
 unzip -q converted/librosa/TheBeatles.zip -d converted/librosa
 unzip -q converted/librosa/USPop179.zip -d converted/librosa
 unzip -q converted/librosa/CaroleKing_Queen.zip -d converted/librosa
-unzip -q converted/mauch/TheBeatles.zip -d converted/mauch
 
 rm -f audio/TheBeatles.zip
 rm -f audio/billboard.zip
@@ -45,6 +44,5 @@ rm -f audio/Jay_Chou.zip
 rm -f converted/librosa/TheBeatles.zip
 rm -f converted/librosa/USPop179.zip
 rm -f converted/librosa/CaroleKing_Queen.zip
-rm -f converted/mauch/TheBeatles.zip
 
 echo OK
